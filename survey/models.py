1da345049540133ca3bb08184d5c50cb2af3f42f
Francis Salinas <francissalinas23@gmail.com>
2014-10-24 11:16:12 -0700

    add the section model in models.py

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Section (models.Model):
+	course = models.Charfield(max_length=4)
+	year = models.Integerfield()
+	section = models.Integerfield()
