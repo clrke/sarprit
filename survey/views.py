78e9404e58428d9d58c6001ae485c72d89cba15f
Francis Salinas <francissalinas23@gmail.com>
2014-10-24 10:32:45 -0700

    Send regine data to index.html

--- survey/views.py
+++ survey/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from sarprit.shortcuts import to_json
 
 def index(request):
-	return render(request, 'survey/index.html')
+	person = {
+		"name" : "Regine", 
+		"age" : 19
+	}
+	return render(request, 'survey/index.html', {"person": person})
 
 def test(request):
 	return to_json([{"data1": "Hello", "data2": "World"}, {"data1": "I", "data2": "am"}, {"data1": "Clarke", "data2": "Plumo"}])
