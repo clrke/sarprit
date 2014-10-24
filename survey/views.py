ceca209379227f111200f98ae494085b37493df5
Maverick Parane <mavericklozadaparane@yahoo.com>
2014-10-24 10:02:08 +0800

    Remove 1 <p> tag Add route to index2.html

--- survey/views.py
+++ survey/views.py
@@ -4,5 +4,8 @@ from sarprit.shortcuts import to_json
 def index(request):
 	return render(request, 'survey/index.html')
 
+def index2(request):
+	return render(request, 'survey/index2.html')
+
 def test(request):
 	return to_json([{"data1": "Hello", "data2": "World"}, {"data1": "I", "data2": "am"}, {"data1": "Clarke", "data2": "Plumo"}])
