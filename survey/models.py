9ed0b4073448d0d77e76194da0909bd4409cfd0a
Francis Salinas <francissalinas23@gmail.com>
2014-10-24 11:34:24 -0700

    Fix improper capitalization

--- survey/models.py
+++ survey/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Section (models.Model):
-	course = models.Charfield(max_length=4)
-	year = models.Integerfield()
-	section = models.Integerfield()
+	course = models.CharField(max_length=10)
+	year = models.IntegerField()
+	section = models.CharField(max_length=3)
