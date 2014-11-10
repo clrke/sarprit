d9c0d3dbcb1f8fee4a32edd62c77d17eb16a44d1
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-10 09:21:15 +0800

    Revert "Revert "add Author, Review and Sentence models in models.py""
    This reverts commit 0bb57761e0592b22cf8e4d2dd92159a997384fac.

--- survey/models.py
+++ survey/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Section (models.Model):
-	course = models.CharField(max_length=10)
-	year = models.IntegerField()
-	section = models.CharField(max_length=3)
+	Course = models.CharField(max_length=10)
+	Year = models.IntegerField()
+	Section = models.CharField(max_length=3)
+
+
+class Author (models.Model):
+	Name = models.CharField(max_length=50)
+	Age = models.IntegerField(max_length=5)
+
+class Review (models.Model):
+	Sentence = models.CharField(max_length=500)
+	
+class Sentence (models.Model):
+	Words = models.CharField(max_length=30)
+	
