430107c8aeb4a415194adcaa447b55fce6fb7e82
Maverick Parane <mavericklozadaparane@gmail.com>
2014-11-10 11:20:24 +0800

    Temporarily add sentence and hashtag splitter

--- global/static/src/js/sarprit.js
+++ global/static/src/js/sarprit.js
@@ -10,4 +10,19 @@ angular.module('SarpritApp', [], function($interpolateProvider) {
 	$http.get('/test').success(function(data) {
 		survey.messages = data;
 	});
+
+	function hasReview() {
+		while((restaurantReview1.length > 0) && (restaurantReview2.length > 0)) {
+			return true;
+		};
+	}
+
+	var sentence1 = "Sarap talaga ng sisig! Kaya nga lang pagkalabas mo, sisig ka na rin! #sisigLord";
+	var sentence2 = "Punta ulit tayo bukas. Pogi talaga ni kuyang waiter! #panalo";
+
+	var result1 = sentence1.match( /((#[\w^#]+ *)|([^\.!\?]+[\.!\?]* *))/g );
+	var result2 = sentence2.match( /((#[\w^#]+ *)|([^\.!\?]+[\.!\?]* *))/g );
+	
+	alert(result2);
+	survey.sentences = result2;
 }]);
