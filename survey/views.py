f655630d5c60a51f26d081278170b9101c6cea25
Francis Salinas <francissalinas23@gmail.com>
2014-11-13 02:10:22 +0800

    add data retrieval

--- survey/views.py
+++ survey/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from sarprit.shortcuts import to_json
 from .models import *
 from django.contrib.auth.decorators import login_required
+from django.forms.models import model_to_dict
+
 
 def index(request):
 	person = {
@@ -18,12 +20,12 @@ def index2(request):
 def test(request):
 	return to_json([{"data1": "Hello", "data2": "World"}, {"data1": "I", "data2": "am"}, {"data1": "Clarke", "data2": "Plumo"}])
 
-@login_required(login_url= 'login/')
+@login_required(login_url= '/admin/login/')
 def sections(request):
 	sections = Section.objects.all()
 	return render(request, 'admin/sections.html', {"sections": sections})
 
-@login_required(login_url= 'login/')
+@login_required(login_url= '/admin/login/')
 def set_section(request,id):
 	for section in Section.objects.all():
 		section.current = False
@@ -34,4 +36,29 @@ def set_section(request,id):
 	section.save();
 
 	return redirect(sections)
+
+
+@login_required(login_url= '/admin/login/')
+def data(request):
+	sections = []
+	for section in Section.objects.all():
+		section2 = model_to_dict(section)
+		students = []
+		sections.append(section2)
+		for student in section.student_set.all():
+			student2 = model_to_dict(student)
+			reviews=[]
+			students.append(student2)
+			for review in student.review_set.all():
+				review2 = model_to_dict(review)
+				sentences=[]
+				reviews.append(review2)
+				for sentence in review.sentence_set.all():
+					sentence2 = model_to_dict(sentence)
+					sentences.append(sentence2)
+				review2['sentences']=sentences
+			student2['reviews']=reviews
+		section2['students']=students
+
+	return to_json(sections)
 
