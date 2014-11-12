787e92414e08d7ec60c119c27a7521947fa29f0c
Francis Salinas <francissalinas23@gmail.com>
2014-11-12 18:04:04 +0800

    add str changes to all models

--- survey/models.py
+++ survey/models.py
@@ -6,15 +6,26 @@ class Section (models.Model):
 	year = models.IntegerField()
 	section = models.CharField(max_length=3)
 
+	def __str__(self):
+		return self.course.upper() + " " + str(self.year) + "-" + self.section.upper()
 
 class Student (models.Model):
 	name = models.CharField(max_length=50)
 	age = models.IntegerField(max_length=3)
 
+	def __str__(self):
+		return self.name.upper() + " " + self.age
+
 class Review (models.Model):
 	sentences = models.CharField(max_length=500)
+
+	def __str__(self):
+		return self.sentences
 	
 class Sentence (models.Model):
 	sentence = models.CharField(max_length=1000)
-	clue = models.CharField(max_length=50)
+	clue = models.CharField(max_length=1)
 	review = models.CharField(max_length=30)
+
+	def __str__(self):
+		return "["+self.clue.upper()+"]" + self.sentence + self.review
