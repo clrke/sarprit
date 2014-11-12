505029a115af29198af9582f79de2392f49dcfe2
Maverick Parane <mavericklozadaparane@gmail.com>
2014-11-12 17:37:01 +0800

    Step 2

--- global/static/src/js/sarprit.js
+++ global/static/src/js/sarprit.js
@@ -6,12 +6,19 @@ angular.module('SarpritApp', [], function($interpolateProvider) {
 	var survey = this;
 	
 	survey.splitReviews = function() {
-		var sentence1 = "Sarap talaga ng sisig! Kaya nga lang pagkalabas mo, sisig ka na rin! #sisigLord";
-		var sentence2 = "Punta ulit tayo bukas. Pogi talaga ni kuyang waiter! #panalo";
+		var sentence1 = survey.review1;
+		var sentence2 = survey.review2;
+		var sentence3 = survey.review3? survey.review3 : "";
+		var sentence4 = survey.review4? survey.review4 : "";
 
 		var result1 = sentence1.match( /((#[\w^#]+ *)|([^\.!\?]+[\.!\?]* *))/g );
 		var result2 = sentence2.match( /((#[\w^#]+ *)|([^\.!\?]+[\.!\?]* *))/g );
+		var result3 = sentence3.match( /((#[\w^#]+ *)|([^\.!\?]+[\.!\?]* *))/g );
+		var result4 = sentence4.match( /((#[\w^#]+ *)|([^\.!\?]+[\.!\?]* *))/g );
 		
-		survey.sentences = result2;
+		survey.sentences1 = result1;
+		survey.sentences2 = result2;
+		survey.sentences3 = result3;
+		survey.sentences4 = result4;
 	}	
 }]);
