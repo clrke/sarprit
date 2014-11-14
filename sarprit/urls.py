4592aeccd9f6d89c864123284cf79ee9cdaf26cf
ArkEidolon <clarke.plumo@gmail.com>
2014-11-14 14:32:04 +0800

    Set correct URL for survey (/survey)

--- sarprit/urls.py
+++ sarprit/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^admin/sections/(?P<id>\d+)$', 'survey.views.set_section'),
     url(r'^admin/data/$', 'survey.views.data'),
     url(r'^$', 'survey.views.index', name='home'),
-    url(r'^2/$', 'survey.views.index2', name='home2'),
+    url(r'^survey/$', 'survey.views.index2', name='home2'),
     url(r'^test/$', 'survey.views.test', name='home'),
 
 )
