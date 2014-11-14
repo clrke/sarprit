ec87dd98d657cb96f6c64d7380c5885c77a05359
ArkEidolon <clarke.plumo@gmail.com>
2014-11-14 12:09:13 +0800

    Add more sentennces

--- sarprit/examples.py
+++ sarprit/examples.py
@@ -3,14 +3,16 @@ def subjective_sentences():
 		"Ang sarap ng pagkain!",
 		"Nakakatuwa sila magserve ng pagkain.",
 		"Hindi ako naiintindihan ng cashier.",
-		"#panalo",
-		"Hindi na ako babalik!",
+		"Ang sarap-sarap ng sisig nila at mura pa, paborito na ito ng tropa ko!"
+		"#panaloSiKuyangWaiter",
+		"Sobrang baho sa lugar nila!",
 	]
 
 def objective_sentences():
 	return [
 		"Ang spaghetti ay isang pagkaing puro pasta.",
 		"You are what you eat.",
+		"Ang mga kinakain mo ay ang kung sino ikaw",
 		"Kumakain kami ngayon sa McDo.",
 		"Here at SM Sta Mesa Food Court!",
 		"Gagala ba tayo ngayon?",
