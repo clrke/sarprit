78b3166ba1e5324e48836b44b64e50e2e232ea40
ArkEidolon <clarke.plumo@gmail.com>
2014-11-14 14:52:57 +0800

    Refactor urls

--- sarprit/urls.py
+++ sarprit/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^admin/sections/$', 'survey.views.sections'),
     url(r'^admin/sections/(?P<id>\d+)$', 'survey.views.set_section'),
     url(r'^admin/data/$', 'survey.views.data'),
+
     url(r'^$', 'survey.views.index', name='home'),
     url(r'^survey/$', 'survey.views.index2', name='home2'),
-    url(r'^test/$', 'survey.views.test', name='home'),
-
 )
