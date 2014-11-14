756881e47bff45288925152287ed3a771f23ec5e
ArkEidolon <clarke.plumo@gmail.com>
2014-11-14 10:40:52 +0800

    Add subjective and objective sentences in examples.py

---
+++ sarprit/examples.py
@@ -0,0 +1,16 @@
+def subjective_sentences():
+	return [
+		"Ang sarap ng pagkain!",
+		"Nakakatuwa sila magserve ng pagkain.",
+		"Hindi ako naiintindihan ng cashier.",
+		"#panalo",
+		"Hindi na ako babalik!",
+	]
+
+def objective_sentences():
+	return [
+		"Ang spaghetti ay isang pagkaing puro pasta.",
+		"You are what you eat.",
+		"Kumakain kami ngayon sa McDo.",
+		"Here at SM Sta Mesa Food Court!"
+	]
