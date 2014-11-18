ca9eba83c2916f8047ac9e2bac184d911b44062f
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-18 12:37:34 +0800

    Add option --nrc (no requirements check) on manage.py

--- manage.py
+++ manage.py
@@ -5,11 +5,15 @@ import sys
 def check_requirements():
 	import django, sklearn
 
-check_requirements()
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sarprit.settings")
 
     from django.core.management import execute_from_command_line
+
+    if "--nrc" not in sys.argv:
+    	print("Checking for requirements. Use 'manage.py --nrc <command>' to skip requirements check.")
+    	check_requirements()
+    else:
+    	sys.argv.remove("--nrc")
 
     execute_from_command_line(sys.argv)
