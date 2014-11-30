9ddf0c6e85d3008b94bd7084d0965164433e4042
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-30 20:16:34 +0800

    Remove bad practice at manage.py

--- manage.py
+++ manage.py
@@ -2,18 +2,9 @@
 import os
 import sys
 
-def check_requirements():
-	import django, sklearn
-
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sarprit.settings")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sarprit.settings")
 
-    from django.core.management import execute_from_command_line
+	from django.core.management import execute_from_command_line
 
-    if "--nrc" not in sys.argv:
-    	print("Checking for requirements. Use 'manage.py --nrc <command>' to skip requirements check.")
-    	check_requirements()
-    else:
-    	sys.argv.remove("--nrc")
-
-    execute_from_command_line(sys.argv)
+	execute_from_command_line(sys.argv)
