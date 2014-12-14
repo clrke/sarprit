413b33f9cb0b49c552adcc06a8979b612a85da07
Ark Eidolon <clarke.plumo@gmail.com>
2014-12-14 15:24:57 +0800

    Fix classifier not found error

--- classifiers/views.py
+++ classifiers/views.py
@@ -15,16 +15,19 @@ def home(request):
 			'classifier4' :  { 'feature_names': classifier4.feature_names, 'data': sorted([(classifier4.features[i], classifier4.target[i]) for i in range(len(classifier4.features))], key=lambda x: x[1]) },
 		})
 def subjectivity(request, id, sentence):
+	from sarprit.examples import classifier1, classifier2, classifier3a, classifier3b, classifier3c, classifier3d, classifier4
 	is_subjective = classifier1.predict([sentence])[0]
 	return to_json({'id': id, 'value': sentence, 'is_subjective': int(is_subjective) is 0, 'features': extract([sentence], classifier1.feature_names)})
 
 def clues(request, id, sentence):
+	from sarprit.examples import classifier1, classifier2, classifier3a, classifier3b, classifier3c, classifier3d, classifier4
 	clue_id = int(classifier2.predict([sentence])[0])
 	clue = classifier2.target_names[clue_id]
 
 	return to_json({'id': id, 'clue': clue, 'clue_id': clue_id, 'features': extract([sentence], classifier2.feature_names)})
 
 def sentiment(request, clue, id, sentence):
+	from sarprit.examples import classifier1, classifier2, classifier3a, classifier3b, classifier3c, classifier3d, classifier4
 	if clue == 'f':
 		classifier = classifier3a
 	elif clue == 'h':
@@ -39,6 +42,7 @@ def sentiment(request, clue, id, sentence):
 	return to_json({'id': id, 'rating': rating, 'features': extract([sentence], classifier.feature_names)})
 
 def overall(request, functional, humanic, mechanic, general):
+	from sarprit.examples import classifier1, classifier2, classifier3a, classifier3b, classifier3c, classifier3d, classifier4
 	rating = classifier4.predict([[float(functional), float(humanic), float(mechanic), float(general)]])[0]
 
 	return to_json({'rating': int(rating)})
