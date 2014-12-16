c3f0d7e9052ddf2edf842e13c15006941fc51823
Francis Salinas <francissalinas23@gmail.com>
2014-12-17 02:47:34 +0800

    create rootword urls in urls.py

--- sarprit/urls.py
+++ sarprit/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 
 import survey.urls
 import classifiers.urls
+import rootword.urls
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^classify/', include(classifiers.urls)),
+    url(r'^rootword/', include(rootword.urls)),
     url(r'', include(survey.urls)),
 )
