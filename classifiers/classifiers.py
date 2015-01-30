806011b9769a496b7652b7088b0d21acbc5e7ce6
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-01-30 16:57:28 +0800

    Remove unneccesary print

--- classifiers/classifiers.py
+++ classifiers/classifiers.py
@@ -31,7 +31,6 @@ class CluesClassifier(MultinomialNB):
 		return super(CluesClassifier, self).fit(self.features, self.target)
 
 	def predict(self, predict_data):
-		print(len(self.features), len(predict_data))
 		feature_names, features = feature_extraction.extract(predict_data, self.feature_names)
 
 		return super(CluesClassifier, self).predict(features)
