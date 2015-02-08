d288ab50ea182dc2165dcaabd10418c9967440dc
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-02-08 15:43:39 +0800

    Make HTMLParser work for both Python 2 and 3

--- twitter/views.py
+++ twitter/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from . import api
 from sarprit.architecture import classify
-from html.parser import HTMLParser
+try:
+	from html.parser import HTMLParser
+except:
+	from HTMLParser import HTMLParser
 import re
 
 def home(request, restaurant):
