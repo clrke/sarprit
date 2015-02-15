e4cb964e67a6b4e15514eb5e4722fa67496f6b78
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-02-15 14:43:55 +0800

    Improve console output of get_accuracy

--- sarprit/statistics.py
+++ sarprit/statistics.py
@@ -64,19 +64,22 @@ def get_accuracy(without_clues = False):
 	f1e = 2 * ((pe * re)/(pe + re))
 	f1n = 2 * ((pn * rn)/(pn + rn))
 
-	print("Precision (Positive):", pp)
-	print("Recall (Positive):", rp)
-	print("F1-score (Positive):", f1p)
+	print("Positive:")
+	print("\tPrecision:", pp)
+	print("\tRecall:   ", rp)
+	print("\tF1-score: ", f1p)
 	print()
 
-	print("Precision (Neutral):", pe)
-	print("Recall (Neutral):", re)
-	print("F1-score (Neutral):", f1e)
+	print("Neutral:")
+	print("\tPrecision:", pe)
+	print("\tRecall:   ", re)
+	print("\tF1-score: ", f1e)
 	print()
 
-	print("Precision (Negative):", pn)
-	print("Recall (Negative):", rn)
-	print("F1-score (Negative):", f1n)
+	print("Negative:")
+	print("\tPrecision:", pn)
+	print("\tRecall:   ", rn)
+	print("\tF1-score: ", f1n)
 	print()
 
 def randomize_review_flags():
