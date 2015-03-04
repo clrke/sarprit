973c7140010bb36479ee47ef0f979437703bccc6
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-03-04 14:36:10 +0800

    Add url to table presentation 1

--- classifiers/urls.py
+++ classifiers/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     url(r'^2/(?P<id>\d+)/(?P<sentence>.+)', views.clues),
     url(r'^3/(?P<clue>\w)/(?P<id>\d+)/(?P<sentence>.+)', views.sentiment),
     url(r'^4/(?P<functional>.+)/(?P<humanic>.+)/(?P<mechanic>.+)/(?P<general>.+)', views.overall),
-    url(r'^presentation', views.presentation),
+    url(r'^presentation$', views.presentation),
+    url(r'^presentation/1$', views.presentation1),
 )
